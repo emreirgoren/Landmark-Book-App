3084d613019a8ab898bdbd5f50cc64cc465a2668
emreirgoren <emreirgoren@outlook.com>
2023-10-22 13:18:56 +0300

    first commit

---
+++ app/src/main/java/com/emreirgoren/landmarkbook/DetailsActivity.java
@@ -0,0 +1,35 @@
+package com.emreirgoren.landmarkbook;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.emreirgoren.landmarkbook.databinding.ActivityDetailsBinding;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        //Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+
+        Singleton singleton = Singleton.getInstance();
+        Landmark selectedLandmark = singleton.getSelectedLandmark();
+
+        binding.landmarkNameText.setText(selectedLandmark.name);
+        binding.countryNameText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+
+    }
+}
